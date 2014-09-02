0ad18059945be245731d3d7c1fd54b8fc92a1444
tommullins <tommullins@10.1.1.250>
2014-09-02 00:04:44 -0700

    testing2

--- TestingApp/src/com/testingapp/MainActivity.java
+++ TestingApp/src/com/testingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.testingapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,8 +13,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w("Hello", "Testing...");
     }    
-
+   
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
